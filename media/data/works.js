881381ea2721c533ddab23b41082d664c2f55907
LoneXHuB <khelifi.louness@live.com>
2021-01-17 11:12:00 +0100

    Second commit

--- media/data/works.js
+++ media/data/works.js
@@ -4,10 +4,10 @@ const works = [
     "desc": "A mini-game I made a gift for someone precious",
     "links": {
       "Source Code": "https://github.com/LoneXHuB/NinoFive",
-      "Screen captures": "https://docs.medamine.now.sh/"
+      "Video": "https://youtu.be/Odw3ajT6zQE"
     },
     "tags": ["CSharp","Unity","Photoshop","Audacity"],
-    "date": "May/2019"
+    "date": "May/2020"
   },
   {
     "title": "Det-website",
@@ -17,17 +17,17 @@ const works = [
       "Website link": "http://det-distribution.dz"
     },
     "tags": ["SpringBoot","javascript","Java","AWS"],
-    "date": "May/2019"
+    "date": "Jan/2020"
   },
   {
     "title": "Tchila virus",
     "desc": "A multiplayer game project i'm currently working on",
     "links": {
       "Source Code": "https://github.com/LoneXHuB/TchilaVirus",
-      "Screen captures": "https://docs.medamine.now.sh/"
+      "Video": "Jan/2021"
     },
     "tags": ["CSharp","Unity","Photoshop","Photon","Audacity"],
-    "date": "May/2019"
+    "date": ""
   },
   {
     "title": "Softbody2D tutorial",
@@ -36,7 +36,7 @@ const works = [
       "Video": "https://www.youtube.com/watch?v=F82BlnW5z6g"
     },
     "tags": ["CSharp","Unity","Photoshop","Audition","Premiere"],
-    "date": "May/2019"
+    "date": "Dec/2020"
   },
   {
     "title": "Realistic portrait drawing",
@@ -46,7 +46,7 @@ const works = [
       "Video": "https://youtu.be/gm_rFGIjPF4"
     },
     "tags": ["Premiere","Art"],
-    "date": "May/2019"
+    "date": "Aug/2017"
   },
   {
     "title": "Det Control tower",
@@ -55,16 +55,26 @@ const works = [
       "Source Code": "https://github.com/LoneXHuB/DetControlTower"
     },
     "tags": ["CSharp","DotNet","MySQL"],
-    "date": "May/2019"
+    "date": "Aug/2019"
   },
   {
     "title": "My wheel",
     "desc": "A vehicule rent website.",
     "links": {
-      "Source Code": "https://github.com/LoneXHuB/myWheel"
+      "Source Code": "https://github.com/LoneXHuB/myWheel",
+      "Screen 0": "https://ibb.co/5KVtkhh",
+      "Screen 1": "https://ibb.co/kSYgJYz",
+      "Screen 2": "https://ibb.co/3pGSpj0",
+      "Screen 3": "https://ibb.co/17HfhMN",
+      "Screen 3": "https://ibb.co/c6qGNLY",
+      "Screen 5": "https://ibb.co/hm4YFLC",
+      "Screen 6": "https://ibb.co/vPxZ4Sm",
+      "Screen 7": "https://ibb.co/mHQmPDC",
+      "Screen 8": "https://ibb.co/PWVKTK9",
+      "Screen 9": "https://ibb.co/P9qrbkn",
     },
     "tags": ["SpringBoot","javascript","MySQL","Java","AWS"],
-    "date": "May/2019"
+    "date": "juin/2018"
   }
 ]
 
